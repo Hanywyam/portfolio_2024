d1a9449779686deda692c1dd8724382f2dd82bc0
Hanjeyoung <142865296+Hanywyam@users.noreply.github.com>
2024-01-06 17:48:07 +0900

    Delete src directory

--- src/pages/BorderContainer.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from "react";
-import Border from "./Border";
-
-const BorderContainer = () => {
-  
-  return <Border scrollWidth={scrollWidth} />;
-};
-
-export default BorderContainer;
